77ed6ca82c0c5f5148aae2f5605d45c953c33c45
Steven Saunders <stevensaunders@stevens-mbp.lan>
2020-01-17 08:08:40 -0700

    adding JS to resize mouse area to fit the adjusted canvas size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,16 +3,33 @@ const canvas = document.querySelector(`canvas`);
 const ctx = canvas.getContext(`2d`);
 const clearBtn = document.querySelector(`button`);
 
-// Get width and height of canvas
-const { width } = canvas.width;
-const { height } = canvas.height;
-
 // Get coordinates on mousedown and mouseenter
 const mousePosition = { x: 0, y: 0 };
 
 function setPosition(e) {
-  mousePosition.x = e.clientX;
-  mousePosition.y = e.clientY;
+  mousePosition.x = e.clientX - canvas.offsetLeft;
+  mousePosition.y = e.clientY - canvas.offsetTop;
+}
+// Create draw function
+function draw(e) {
+  if (e.buttons !== 1) {
+    return;
+  }
+
+  // /Begin the path
+  ctx.beginPath();
+  // /Set line properties
+  ctx.lineCap = `round`;
+  ctx.lineJoin = `round`;
+  ctx.lineWidth = 10;
+  ctx.strokeStyle = `#672293`;
+  ctx.stroke();
+
+  // /Draw on canvas
+  ctx.moveTo(mousePosition.x, mousePosition.y);
+  ctx.lineTo(mousePosition.x, mousePosition.y);
+
+  // /Set flag for mouseup
 }
 
 // Attach mouse down, mousemove, and mouseup event listeners to the canvas
@@ -20,19 +37,6 @@ canvas.addEventListener(`mousemove`, draw);
 canvas.addEventListener(`mousedown`, setPosition);
 canvas.addEventListener(`mouseenter`, setPosition);
 
-// Create draw function
-function draw() {}
-
-// /Begin the path
-
-// /Set line properties
-
-// /Draw on canvas
-
-// /Set flag for mouseup
-
 // create clear canvas function
 
 // add event listener to button
-
-//
